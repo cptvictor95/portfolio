0668ebd6c297f34f5a9c35c36af5b6b824b50bc4
cptvictor95 <cpt.victor@hotmail.com>
2023-06-13 07:02:22 -0300

    fix: remove projects link from header

--- locale/header.ts
+++ locale/header.ts
@@ -1,25 +1,23 @@
 export const ptBR = {
   blogLinks: [
-    { name: 'home', to: '', id: 5 },
-    { name: 'artigos', to: 'blog', id: 6 },
+    { name: 'home', to: '', id: 4 },
+    { name: 'artigos', to: 'blog', id: 5 },
   ],
   homeLinks: [
     { name: 'sobre', to: 'about', id: 1 },
-    { name: 'projetos', to: 'projects', id: 2 },
-    { name: 'contato', to: 'contact', id: 3 },
-    { name: 'artigos', to: 'blog', id: 4 },
+    { name: 'contato', to: 'contact', id: 2 },
+    { name: 'artigos', to: 'blog', id: 3 },
   ],
 };
 
 export const enUS = {
   blogLinks: [
-    { name: 'home', to: '', id: 5 },
-    { name: 'blog', to: 'blog', id: 6 },
+    { name: 'home', to: '', id: 4 },
+    { name: 'blog', to: 'blog', id: 5 },
   ],
   homeLinks: [
     { name: 'about', to: 'about', id: 1 },
-    { name: 'projects', to: 'projects', id: 2 },
-    { name: 'contact', to: 'contact', id: 3 },
-    { name: 'blog', to: 'blog', id: 4 },
+    { name: 'contact', to: 'contact', id: 2 },
+    { name: 'blog', to: 'blog', id: 3 },
   ],
 };
